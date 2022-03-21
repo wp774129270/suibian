3f1e9bd4873fd8921b94510d4036b5d7912c7bc9
阿愔 <774129270@qq.com>
2022-03-21 20:03:39 +0800

    完成了用户列表功能开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import '../css/glo.css'
 Vue.use(ElementUI)
 // Vue.prototype.$message = Message
--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Login from '../components/Login.vue'
 import Page from '../components/Page.vue'
 import Home from '../components/Home.vue'
 import Welcome from '../components/Welcome.vue'
+import Users from '../components/user/Users.vue'
 Vue.use(VueRouter)
 
 const router = new VueRouter({
@@ -22,11 +23,16 @@ const router = new VueRouter({
     {
       path: '/home',
       component: Home,
-      redirect:'/welcome',
+      redirect: '/welcome',
       children: [{
-        path: '/welcome',
-        component: Welcome
-      }]
+          path: '/welcome',
+          component: Welcome
+        },
+        {
+          path: '/users',
+          component: Users
+        }
+      ]
     }
 
   ]
